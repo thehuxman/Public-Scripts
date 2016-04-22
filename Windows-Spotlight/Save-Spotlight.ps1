eb83a94a4c284e26ea7e76df0d06df8768ac8e64
thehuxman <michael.huxley@mickhuxley.com>
2016-04-22 13:53:06 +1000

    Create Save-Spotlight.ps1

---
+++ Windows-Spotlight/Save-Spotlight.ps1
@@ -0,0 +1,92 @@
+Function Get-FileMetaData 
+{ 
+  <# 
+   .Synopsis 
+    This function gets file metadata and returns it as a custom PS Object  
+   .Description 
+    This function gets file metadata using the Shell.Application object and 
+    returns a custom PSObject object that can be sorted, filtered or otherwise 
+    manipulated. 
+   .Example 
+    Get-FileMetaData -folder "e:\music" 
+    Gets file metadata for all files in the e:\music directory 
+   .Example 
+    Get-FileMetaData -folder (gci e:\music -Recurse -Directory).FullName 
+    This example uses the Get-ChildItem cmdlet to do a recursive lookup of  
+    all directories in the e:\music folder and then it goes through and gets 
+    all of the file metada for all the files in the directories and in the  
+    subdirectories.   
+   .Example 
+    Get-FileMetaData -folder "c:\fso","E:\music\Big Boi" 
+    Gets file metadata from files in both the c:\fso directory and the 
+    e:\music\big boi directory. 
+   .Example 
+    $meta = Get-FileMetaData -folder "E:\music" 
+    This example gets file metadata from all files in the root of the 
+    e:\music directory and stores the returned custom objects in a $meta  
+    variable for later processing and manipulation. 
+   .Parameter Folder 
+    The folder that is parsed for files  
+   .Notes 
+    NAME:  Get-FileMetaData 
+    AUTHOR: ed wilson, msft 
+    LASTEDIT: 01/24/2014 14:08:24 
+    KEYWORDS: Storage, Files, Metadata 
+    HSG: HSG-2-5-14 
+   .Link 
+     Http://www.ScriptingGuys.com 
+ #Requires -Version 2.0 
+ #> 
+ Param([string[]]$folder) 
+ foreach($sFolder in $folder) 
+  { 
+   $a = 0 
+   $objShell = New-Object -ComObject Shell.Application 
+   $objFolder = $objShell.namespace($sFolder) 
+ 
+   foreach ($File in $objFolder.items()) 
+    {  
+     $FileMetaData = New-Object PSOBJECT 
+      for ($a ; $a  -le 266; $a++) 
+       {  
+         if($objFolder.getDetailsOf($File, $a)) 
+           { 
+             $hash += @{$($objFolder.getDetailsOf($objFolder.items, $a))  = 
+                   $($objFolder.getDetailsOf($File, $a)) } 
+            $FileMetaData | Add-Member $hash 
+            $hash.clear()  
+           } #end if 
+       } #end for  
+     $a=0 
+     $FileMetaData 
+    } #end foreach $file 
+  } #end foreach $sfolder 
+} #end Get-FileMetaData
+
+#region Copy to Staging Folder and Clean Up
+$date = Get-Date -Format "dd MMM yyyy"
+$newdir = "$env:USERPROFILE\Pictures\Spotlight\$date"
+
+new-item $newdir -ItemType Directory -Force
+
+Copy-Item -Path "$env:LOCALAPPDATA\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets\*" -Destination $newdir -Recurse
+
+Get-ChildItem -Path $newdir -Recurse | ?{!$_.PsIsContainer} | ren -new {$_.name + ".jpg"} 
+
+
+$Pics = Get-FileMetaData -folder $newdir | % {
+$PicName = $_.Name
+if ($_.width -ne "1920 pixels") {
+
+
+remove-item -Path "$newdir\$PicName"
+
+}
+
+}
+
+#endregion
+
+Copy-Item -Path $newdir\* -Destination "$env:USERPROFILE\OneDrive\Personal Documents\Background Images" -Recurse | Out-Null ##Update to your location
+Remove-Item -Path $newdir -Force -Confirm:$false -Recurse
+
